6403022019fab9f5756951eb28c9bfff9f280045
Sayali Powar <powarsayali2002@gmail.com>
2024-09-02 01:14:40 +0530

    Fertilizers company dashboard updated

--- front_end/front_app/urls.py
+++ front_end/front_app/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
    path('index.html',views.home,name="home"),
    path('profile.html',views.profile,name="profile"),
    path('add_stock.html',views.add_stock,name="add_stock"),
-   path('Businessman_dashboad.html',views.Businessman_dashboad,name="Businessman_dashboad"),
+   path('fertilizer_company_dashboard',views.fertilizer_company_dashboard,name="fertilizer_company_dashboard"),
+   path('update_company',views.update_company,name="update_company"),
+   path('add_requirement',views.add_requirement,name="add_requirement"),
+   path('view_my_order',views.view_my_order,name="view_my_order"),
+   path('company_contact',views.company_contact,name="company_contact"),
 ]
 
 
--- front_end/front_app/views.py
+++ front_end/front_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 
-
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -11,12 +10,23 @@ def profile(request):
 def add_stock(request):
     return render(request,'add_stock.html')
 
-def Businessman_dashboad(request):
+def fertilizer_company_dashboard(request):
     fish_list = [
         {'name': 'Salmon', 'location': 'Norway', 'quantity': '10 kg', 'price': '1000 rs', 'address': 'Oslo, Norway', 'phone': '+47 123 456 78'},
         {'name': 'Tuna', 'location': 'Japan', 'quantity': '5 kg', 'price': '1500 rs', 'address': 'Tokyo, Japan', 'phone': '+81 123 456 78'},
         {'name': 'Cod', 'location': 'Iceland', 'quantity': '8 kg', 'price': '800 rs', 'address': 'Reykjavik, Iceland', 'phone': '+354 123 456'},
     ]
-    return render(request, 'Businessman_dashboad.html', {'fish_list': fish_list})
-    
+    return render(request, 'fertilizer_company_dashboard.html', {'fish_list': fish_list})
+
+def update_company(request):
+    return render(request, 'update_company.html')
+
+def company_contact(request):
+    return render(request,'company_contact.html') 
+
+def add_requirement(request):
+    return render(request, 'add_requirement.html')
+
+def view_my_order(request):
+    return render(request, 'view_my_order.html')
 
